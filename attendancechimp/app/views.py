8c80980a4651a5763838c1d9fd0eee1199356295
Khadijat Durojaiye <135638401+khadarty@users.noreply.github.com>
2024-10-17 16:19:53 -0500

    fixyyyy

--- attendancechimp/app/views.py
+++ attendancechimp/app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
+from datetime import datetime
+import datetime
 
 @csrf_exempt
 def index(request):
