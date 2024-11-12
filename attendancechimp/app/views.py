bbded09b63b689a5b2a1017406fbfa5886cf5f8c
Khadijat Durojaiye <135638401+khadarty@users.noreply.github.com>
2024-11-12 12:36:49 -0600

    heyyyyy

--- attendancechimp/app/views.py
+++ attendancechimp/app/views.py
@@ -58,15 +58,15 @@ def createUser(request):
         password = request.POST.get('password')
         is_student = request.POST.get('is_student') == '1'
 
-        # Check if user with email already exists
+       
         if User.objects.filter(email=email).exists():
             return JsonResponse({'error': 'A user with this email already exists.'}, status=400)
 
-        # Create the user
+        
         user = User.objects.create_user(username=user_name, email=email, password=password)
         user.save()
 
-        # Return success response
+        
         return JsonResponse({'success': 'User created successfully.'}, status=200)
     
     return JsonResponse({'error': 'Invalid request method.'}, status=405)
@@ -101,3 +101,37 @@ def index(request):
     
 #     return render(request, 'index.html', context)
     
+@csrf_exempt
+def create_course(request):
+    if not request.user.is_authenticated or not request.user.is_instructor:
+        return HttpResponse(status=401)
+    if request.method == "POST":
+        course_name = request.POST.get("course-name")
+        start_time = request.POST.get("start-time")
+        end_time = request.POST.get("end-time")
+        days = [day for day in ["mon", "tue", "wed", "thu", "fri"] if request.POST.get(f"day-{day}")]
+        
+        Course.objects.create(name=course_name, start_time=start_time, end_time=end_time, days=days)
+        return JsonResponse({"status": "Course created"}, status=201)
+    return HttpResponse(status=405)
+
+@csrf_exempt
+def create_lecture(request):
+    if not request.user.is_authenticated or not request.user.is_instructor:
+        return HttpResponse(status=401)
+    if request.method == "POST":
+        course_id = request.POST.get("choice")
+        course = Course.objects.get(id=course_id)
+        Lecture.objects.create(course=course)
+        return JsonResponse({"status": "Lecture created"}, status=201)
+    return HttpResponse(status=405)
+
+@csrf_exempt
+def create_qr_upload(request):
+    if not request.user.is_authenticated or not request.user.is_student:
+        return HttpResponse(status=401)
+    if request.method == "POST":
+        image = request.FILES.get("imageUpload")
+        QRCodeUpload.objects.create(user=request.user, image=image)
+        return JsonResponse({"status": "Upload created"}, status=201)
+    return HttpResponse(status=405)
