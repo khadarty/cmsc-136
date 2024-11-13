fd75a9ccd9e8a4a195d11b62b84cae467eb3edd3
Delicia Chen <delchen@uchicago.edu>
2024-11-13 11:08:08 -0600

    HW5

--- attendancechimp/app/urls.py
+++ attendancechimp/app/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('new_course', views.new_course, name='course_create'), # course_create: localhost:8000/app/new_course
     path('new_lecture', views.new_lecture, name='qr_create'), # qr_create: localhost:8000/app/new_lecture
     path('new_qr_upload', views.new_qr_upload, name='qr_upload'), # qr_upload: localhost:8000/new_qr_upload
-    path('createCourse', views.createCourse, name='create_course'),
-    path('createLecture', views.createLecture, name='create_qr'),
-    path('createQRCodeUpload', views.createQRCodeUpload, name='upload_qr'),
+    path('createCourse/', views.createCourse, name='create_course'),
+    path('createLecture/', views.createLecture, name='create_qr'),
+    path('createQRCodeUpload/', views.createQRCodeUpload, name='upload_qr'),
     path('dumpUploads', views.dumpUploads, name='dump_uploads')
 ]
